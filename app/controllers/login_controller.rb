e806de0b23bcf3bbcd303272ee5a3b13ffc26835
Анастасия Шагалова <shagalova.stasya@gmail.com>
2022-12-16 20:10:56 +0300

    update register

--- app/controllers/login_controller.rb
+++ app/controllers/login_controller.rb
@@ -7,7 +7,7 @@ class LoginController < ApplicationController
     user = Agent.find_by_email(user_params[:email])
     if user.present? && user.authenticate(user_params[:password])
       session[:user_id] = user.id
-      redirect_to '/home'
+      redirect_to root_path
     else
       flash[:error] = ['Invalid Credentials']
       redirect_to "/login"
--- app/controllers/register_controller.rb
+++ app/controllers/register_controller.rb
@@ -7,7 +7,7 @@ class RegisterController < ApplicationController
     user = Agent.new(user_params)
     if user.save
       session[:user_id] = user.id
-      redirect_to "/agent#{user.id}"
+      redirect_to "/agent/#{user.id}"
     else
       flash[:error] = user.errors.full_messages
       redirect_to '/register'
